97ea912ea6617c46ce4f51866ba0b50a2596f036
David Caffrey <dave.caffrey71@gmail.com>
2023-02-08 06:18:28 +0000

    button functionality added

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,8 @@ const editPlayer2Btn = document.getElementById("edit-player-2-btn");
 
 const playerConfigOverlayElement = document.getElementById("config-overlay");
 const backdropElement = document.getElementById("backdrop");
+const formElement = document.querySelector("form");
+const errorsOutputElement = document.getElementById("config-errors");
 
 const cancelConfigBtnElement = document.getElementById("cancel-config-btn");
 const confirmConfigBtnElement = document.getElementById("confirm-config-btn");
@@ -13,3 +15,5 @@ editPlayer2Btn.addEventListener("click", openPlayerConfig);
 cancelConfigBtnElement.addEventListener("click", closePlayerConfig);
 confirmConfigBtnElement.addEventListener("click", confirmPlayerConfig);
 backdropElement.addEventListener("click", closePlayerConfig);
+
+formElement.addEventListener("submit", savePlayerConfig);
--- scripts/config.js
+++ scripts/config.js
@@ -6,8 +6,24 @@ function openPlayerConfig() {
 function closePlayerConfig() {
   playerConfigOverlayElement.style.display = "none";
   backdropElement.style.display = "none";
+  formElement.firstElementChild.classList.remove("error");
+  errorsOutputElement.textContent = "";
 }
 
 function confirmPlayerConfig() {
   alert("heelo dave");
+}
+
+function savePlayerConfig(event) {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const enteredPlayerName = formData.get("playername").trim();
+  //empty string is considered falsey
+  if (!enteredPlayerName) {
+    // alert("please enter a name")
+    event.target.firstElementChild.classList.add("error");
+    errorsOutputElement.textContent = "please enter a valid name";
+    return;
+  }
 }
